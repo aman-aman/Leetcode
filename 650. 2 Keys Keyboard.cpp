0c67fdc2328fcaaf54fae3bd19953a01ada87250
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-12-22 08:12:42 +0530

    Add files via upload

---
+++ 650. 2 Keys Keyboard.cpp
@@ -0,0 +1,23 @@
+//aman kumar jha
+class Solution {
+public:
+    int minSteps(int n)
+    {
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=i;
+            for(int j=i-1;j>1;j--)
+            {
+                if(i%j==0)
+                {
+                    dp[i]=dp[j]+i/j;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
