a61ce0ad756a4981b36acb0a11cc45d8df758e1c
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-12-22 23:21:38 +0530

    Add files via upload

---
+++ 413. Arithmetic Slices.cpp
@@ -0,0 +1,24 @@
+//aman kumar jha
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& a)
+    {
+        int n=a.size();
+        if(n<3)
+            return 0;
+
+        int dp[n];
+        memset(dp,0,sizeof(dp));
+        int res=0;
+        for(int i=2;i<n;i++)
+        {
+            if((a[i]-a[i-1]==a[i-1]-a[i-2]))
+            {
+                dp[i]=dp[i-1]+1;
+            }
+            res=res+dp[i];
+        }
+
+        return res;
+    }
+};
