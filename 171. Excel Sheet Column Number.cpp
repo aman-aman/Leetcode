7e141f1b2c1337ae62126ebf0d4bd2d5e86138ff
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-01 22:15:22 +0530

    Add files via upload

---
+++ 171. Excel Sheet Column Number.cpp
@@ -0,0 +1,13 @@
+//aman kumar jha
+class Solution {
+public:
+    int titleToNumber(string s)
+    {
+        int num=0;
+        for(int i=0;i<s.length();i++)
+        {
+            num=num*26+s[i]-'A'+1;
+        }
+        return num;
+    }
+};
