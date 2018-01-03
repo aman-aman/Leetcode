0e9ba066a17be73026739d6dfa0425ec8a0b25f4
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-03 22:55:36 +0530

    Add files via upload

---
+++ 172. Factorial Trailing Zeroes.cpp
@@ -0,0 +1,12 @@
+//aman kumar jha
+class Solution {
+public:
+    int trailingZeroes(int n)
+    {
+        int count=0;
+        for (long long i=5; n/i>=1; i *= 5)
+          count += n/i;
+
+        return count;
+    }
+};
