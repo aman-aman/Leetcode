6cb495819ab87390ba753956ee2c6d43316fc82e
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-04 19:25:02 +0530

    Add files via upload

---
+++ 389. Find the Difference.cpp
@@ -0,0 +1,20 @@
+//aman kumar jha
+class Solution {
+public:
+    char findTheDifference(string s, string t)
+    {
+        vector<int> cnt(26,0);
+        for(int i=0;i<s.length();i++)
+            cnt[s[i]-'a']++;
+
+        for(int i=0;i<t.length();i++)
+            cnt[t[i]-'a']--;
+
+        for(int i=0;i<26;i++)
+        {
+            if(cnt[i]!=0)
+                return (char)(i+'a');
+        }
+
+    }
+};
