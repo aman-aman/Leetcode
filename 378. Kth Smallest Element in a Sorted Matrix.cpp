3164bb2f374bcab7790e8dd4ac9c8371d1485d24
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-05 16:44:47 +0530

    Add files via upload

---
+++ 378. Kth Smallest Element in a Sorted Matrix.cpp
@@ -0,0 +1,24 @@
+//aman kumar jha
+class Solution
+{
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k)
+    {
+        priority_queue<int,vector<int> ,greater<int> > pq;
+
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                pq.push(matrix[i][j]);
+            }
+        }
+        int val;
+        while(k--)
+        {
+            val=pq.top();
+            pq.pop();
+        }
+        return val;
+    }
+};
