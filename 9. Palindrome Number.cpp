f9cc691e1d590553018ecf9599003c88e96c585b
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-09 18:23:34 +0530

    Add files via upload

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,12 @@
+//aman kumar jha
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        string s=to_string(x);
+        string temp=s;
+        reverse(temp.begin(),temp.end());
+        return temp==s;
+
+    }
+};
