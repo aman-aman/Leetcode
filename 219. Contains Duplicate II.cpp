7fd9107dda2559a59438b3018302c2feb463c6b6
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-10 10:33:23 +0530

    Add files via upload

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,20 @@
+//aman kumar jha
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k)
+    {
+        if(k==0)
+            return false;
+
+        set<int> s;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i>k)
+                s.erase(nums[i-k-1]);
+            if(s.find(nums[i])!=s.end())
+                return true;
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
