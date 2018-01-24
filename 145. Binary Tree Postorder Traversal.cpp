0134bf0d376769a98886770c797e6e79ba7ee385
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-24 17:23:39 +0530

    Add files via upload

---
+++ 145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,23 @@
+//aman kumar jha
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    vector<int> postorderTraversal(TreeNode* root)
+    {
+        if(!root)
+            return res;
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        res.push_back(root->val);
+        return res;
+    }
+};
